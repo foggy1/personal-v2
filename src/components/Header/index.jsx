3ba052263dde79834262247449d0eab92ec5f10a
Austin Lanari <austin@jumanji.io>
2019-01-19 09:18:27 -0800

    add header; remove sidebar; tighten index and widen content

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Header extends Component {
+  render () {
+    return (
+      <header>
+        <h1>Austin Lanari</h1>
+      </header>
+    )
+  }
+}
+
+export default Header
