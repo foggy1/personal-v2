d00e0aff5563f5959590ab523c439f1be68b41de
Austin Lanari <austin@jumanji.io>
2019-02-01 08:42:01 -0800

    day date bubbles

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import moment from 'moment'
 import './style.scss'
+import feather from 'feather-icons'
 
 class Post extends React.Component {
   render() {
@@ -15,29 +16,29 @@ class Post extends React.Component {
 
     return (
       <div className="post">
-
-          <h3 className="post__title">
-          <span className="post__meta">
+        <div className="post__title-row">
             <time
               className="post__meta-time"
               dateTime={moment(date).format('MMMM D, YYYY')}
+              style={{position: 'relative'}}
             >
-              {moment(date).format('DD')}
+              <i dangerouslySetInnerHTML={{ __html: feather.icons.calendar.toSvg({height: 36, width: 36, color: 'white', zIndex: 10}) }} />
+              <span style={{position: 'absolute', bottom: 5, left: 12, backgroundColor: '#5d93ff', color: 'white', lineHeight: '40px', width: 40, borderRadius: 50, textAlign: 'center'}}>{moment(date).format('DD')}</span>
             </time>
-            <span className="post__meta-divider" />
-          </span>
+            <span classNme="post__meta-divider" />
+          <h3 className="post__title">
             <Link className="post__title-link" to={slug}>
               {title}
             </Link>
           </h3>
-            <div className="post__meta">
+          {/*            <div className="post__meta">
             <span className="post__meta-category" key={categorySlug}>
               <Link to={categorySlug} className="post__meta-category-link">
                 {category}
               </Link>
             </span>
-          </div>
-
+          </div>*/}
+        </div>
         <p className="post__description">{description}</p>
       </div>
     )
