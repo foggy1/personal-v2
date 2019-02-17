a2fad8595416aa6e3fe663a1dd1ae2eaba41b592
Austin Lanari <austin@jumanji.io>
2019-02-16 17:19:39 -0800

    update image to fluid and add fig caption

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,7 +12,7 @@ class IndexRoute extends React.Component {
     const items = []
     const { title, subtitle } = this.props.data.site.siteMetadata
     const groups = this.props.data.allMarkdownRemark.group
-    const image = this.props.data.imageSharp.fixed
+    const image = this.props.data.imageSharp.fluid
     const sections = groups.map(({fieldValue, edges: posts}) => (
       <div>
         <h2>{moment(fieldValue).format('MMMM YYYY')}</h2>
@@ -33,8 +33,11 @@ class IndexRoute extends React.Component {
           </Helmet>
           <div className="content">
             <div className="content__top">
-              <p>I'm an open-source developer and the lead software engineer at <a href="https://vody.com" target="_blank">Vody</a>. If you have a non-profit or labor-focused software need, <a href="mailto:austin@jumanji.io?subject=Potential software project&body=Hi Austin,%0A%0AI'm reaching out on behalf of [you or your organization] for help with a project.%0A%0A[Briefly describe your needs here.]">let's talk about it.</a></p>
-              <Img style={{margin: "0 auto", display: 'block'}}className="content__top__image" fixed={image} />
+              <p>I'm an open-source developer and the lead software engineer at <a href="https://vody.com" target="_blank">Vody</a>. If you have a non-profit or labor-focused software need, <a href="mailto:austin@jumanji.io?subject=Potential software project&body=Hi Austin,%0A%0AI'm reaching out on behalf of [your name or your organization's name'] for help with a project.%0A%0A[Briefly describe your needs here.]">let's talk about it.</a></p>
+              <div style={{maxHeight: 510}}>
+                <Img alt="selfie of only the side of me wearing a hat with Mark Rothko No. 301 painting behind me" className="content__top__image" fluid={image} />
+                <figcaption classname="content__top__caption">The side of my face in my favorite room in L.A., MOCA's Rothko room. Rothko's <em>No. 301</em> behind me.</figcaption>
+              </div>
             </div>
             <div className="content__inner">{sections}</div>
           </div>
@@ -66,8 +69,8 @@ export const pageQuery = graphql`
       }
     }
     imageSharp(original:{src: {regex: "/\/static\/me_and_rothko-/"}}) {
-      fixed(width: 340) {
-        ...GatsbyImageSharpFixed_withWebp_tracedSVG
+      fluid(maxWidth: 510, maxHeight: 680) {
+        ...GatsbyImageSharpFluid_withWebp_tracedSVG
       }
     }
     allMarkdownRemark(
